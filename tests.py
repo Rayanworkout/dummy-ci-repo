f1c52e8ededd4d3f3e2d2fba19adf3a48983b6fb
rayan <rayan13170@protonmail.com>
2024-03-04 20:13:43 +0100

    added dummy tests

---
+++ tests.py
@@ -0,0 +1,18 @@
+def test_1():
+    assert 1 == 1
+
+
+def test_2():
+    assert 2 == 2
+
+
+def test_3():
+    assert 3 == 3
+
+
+def test_4():
+    assert 4 == 4
+
+
+def test_5():
+    assert 5 == 5
